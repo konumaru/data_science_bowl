268d9ef7143c9358fa0cbad23ac87e86910790b1
Rui Konuma <17187586+louis1022@users.noreply.github.com>
2020-01-31 02:06:38 +0900

    [update] ディレクトリ構造を更新

---
+++ docs/ranker_solutions.md
@@ -0,0 +1,71 @@
+# メダル圏内のスコアが取れるnotebookを作る
+
+- メダル圏内の人の特徴量を真似して、自分のpiplineを組み、メダル圏内のスコアを出す。
+- また、try and error がうまくいく方法を身につける。
+- 一旦、決定木系の解法だけを参考にする。
+- rank solutions
+  - https://kaggledb.com/solutions
+
+
+
+## メダルスコアを取るまでの手順
+
+- もう一度、最も簡単な自分のpiplineを作る。
+- メダル圏内カーネルを１つずつ参照していき、特徴量を追加する。
+- 途中でコードをリファクタリングしたくなったら積極的に行う。
+
+
+
+# 参考カーネル
+
+## アライさんのnotebook
+
+kernel: https://www.kaggle.com/hidehisaarai1213/dsb2019-nn-ovr-reduce-90-val-60-percentile#Functions-and-Classes
+
+
+### 特徴量
+- `past_summary_features`関数がメインで特徴量を作っている部分
+- 基本の処理方法は、installation_id, sessionごとに処理するやつで同じ。
+- session_typeごとに処理を記述
+  - 更に、session_titleで分岐
+
+
+### 学習方法
+- 回帰予測のplossに最適化処理を使う
+```
+def lgb_regression_qwk(y_pred: np.ndarray, data: lgb.Dataset) -> Tuple[str, float, bool]:
+    y_true = (data.get_label() * 3).astype(int)
+    y_pred = y_pred.reshape(-1)
+
+    OptR = OptimizedRounder(n_classwise=3, n_overall=3)
+    OptR.fit(y_pred, y_true)
+
+    y_pred = OptR.predict(y_pred).astype(int)
+    qwk = calc_metric(y_true, y_pred)
+
+    return "qwk", qwk, True
+```
+
+
+
+
+
+
+# MEMO
+
+## 知らない単語
+- truncated, truncated train data
+  - 学習時に各installation_idのデータを１つに絞ること。
+- null importance
+  - ラベルデータをシャッフルしたときのfeature importance。
+  - shuffle後のfeature importanceと同じくらい、あるいは低いのであればランダム値よりも重要ではない可能性が高い。
+
+## 略語・変数名の処理について
+- コンペごとの略語変数名は、処理部分の頭にリストにしてくと良さそう。
+  - ex) sess -> game_session, session
+
+## adversarial validation
+- あるinstallation_idにつき、１つのAssessmentのデータをランダムサンプリングする。
+- それを5-foldで学習する。
+  - 1st のAUCは、0.49 ~ 0.51 に落ち着いた。
+  - feature importanceがリークしているとAUCが高くなる傾向がある。
---
+++ v01000/reduced_data.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+ls_file_name = os.listdir()
+os.chdir(os.path.abspath('../../'))
+
+
+print(ls_file_name)
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)\n'.format(
+        end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+
+SRC_FILES = [
+    'sample_submission.csv',
+    'specs.csv',
+    'test.csv',
+    'train_labels.csv',
+    'train.csv'
+]
+
+
+for src_file in SRC_FILES:
+    print(f'Processing {src_file}')
+    raw_df = pd.read_csv('./data/raw/' + src_file)
+    reduced_df = reduce_mem_usage(raw_df)
+    reduced_df.to_csv('./data/reduced/' + src_file, index=False)
